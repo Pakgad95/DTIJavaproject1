8bf5382cb0b50b9f64968caa9935ff9bd445901e
Pakgad95 <s6752410029@sau.ac.th>
2025-01-14 14:22:58 +0700

    V1 class02

---
+++ src/com/sau/tech/Myclass00.java
@@ -0,0 +1,8 @@
+package com.sau.tech;
+
+public class Myclass00 {
+    //เมธอด main
+    public static void main(String[] args) {
+        System.out.println("Hello,World");
+    }
+}
--- src/com/sau/tech/Myclass02.java
+++ src/com/sau/tech/Myclass02.java
@@ -1,4 +1,8 @@
 package com.sau.tech;
 
 public class Myclass02 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("Hi. world");
+    }
 }
