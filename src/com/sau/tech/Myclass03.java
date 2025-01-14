3c47ffbfc9a43a994405c5cbab489901768930e5
Pakgad95 <s6752410029@sau.ac.th>
2025-01-14 17:09:36 +0700

    22 Edit Mylass03 for my user input

--- src/com/sau/tech/Myclass03.java
+++ src/com/sau/tech/Myclass03.java
@@ -15,9 +15,9 @@ public class Myclass03 {
         System.out.print("Input employee Salary : ");
         empSalary = sc.nextDouble();
 
-        System.out.print("----------------------");
+        System.out.println("----------------------");
 
-        tax10 = empSalary * 10-100;
+        tax10 = empSalary * 10 / 100;
         System.out.println("Your name : " + empName);
         System.out.println("Your salary : " + empSalary);
         System.out.println("Tax Pay : " + tax10 + "Bath");
